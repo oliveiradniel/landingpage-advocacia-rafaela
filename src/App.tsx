fb00b5ba53d17e8c8bf72a08bc4a6149bf920eeb
oliveiradniel <kadadniel@gmail.com>
2025-06-15 16:04:37 -0300

    feat: add ReviewsSection

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import HeroSection from "./sections/HeroSection";
+import ReviewsSection from "./sections/ReviewsSection";
 import SpecialtiesSection from "./sections/SpecialtiesSection";
 import WhyChooseMySection from "./sections/WhyChooseMySection";
 
 function App() {
   return (
-    <div className="bg-">
+    <div>
       <HeroSection />
       <WhyChooseMySection />
       <SpecialtiesSection />
+      <ReviewsSection />
     </div>
   );
 }
---
+++ src/sections/ReviewsSection.tsx
@@ -0,0 +1,73 @@
+import { FaUser } from "react-icons/fa";
+import { IoMdStar } from "react-icons/io";
+
+export default function ReviewsSection() {
+  const mockDepoiments = [
+    {
+      id: Math.random(),
+      name: "Camila Ferreira",
+      avatar_url: "/camila-ferreira.jpg",
+      numberOfStars: 4,
+      assessment:
+        "Profissional incrível! Resolveu meu caso com uma rapidez que eu nem esperava. Consegui receber todos os meus direitos sem dor de cabeça.",
+    },
+    {
+      id: Math.random(),
+      name: "Carlos Alberto",
+      avatar_url: "/carlos-alberto.jpg",
+      numberOfStars: 5,
+      assessment:
+        "Muito atenciosa e dedicada. Me explicou tudo com uma linguagem simples e me deixou segura durante todo o processo. Consegui a indenização que precisava e me senti muito acolhida.",
+    },
+    {
+      id: Math.random(),
+      name: "Patrícia Souza",
+      avatar_url: "/patricia-souza.jpg",
+      numberOfStars: 5,
+      assessment:
+        "A forma como ela conduziu meu processo foi excepcional! Além da parte técnica, senti de verdade que ela se importava com a minha história.",
+    },
+  ];
+
+  return (
+    <div className="pt-32 flex items-center flex-col">
+      <h1 className="text-[32px] font-inter font-bold">
+        O que <span className="text-vivid-blue">nossos cliente</span> dizem
+      </h1>
+
+      <div className="mt-8 flex gap-4">
+        {mockDepoiments.map(
+          ({ id, name, avatar_url, numberOfStars, assessment }) => (
+            <div
+              key={id}
+              className="rounded-lg w-[350px] border flex flex-col border-white/10 p-4 gap-2 shadow-[0_8px_20px_rgba(0,0,0,0.3)]"
+            >
+              <p className="h-[160px]">{assessment}</p>
+
+              <div className="flex items-center gap-2">
+                {avatar_url && avatar_url?.length > 0 ? (
+                  <img
+                    src={avatar_url}
+                    alt="Foto de Perfil"
+                    className="w-[42px] h-[42px] rounded-full object-cover"
+                  />
+                ) : (
+                  <FaUser size={28} height={28} />
+                )}
+
+                <div className="flex flex-col">
+                  <p className="font-lato font-bold">{name}</p>
+                  <div className="flex">
+                    {Array.from({ length: numberOfStars }).map((_, i) => (
+                      <IoMdStar key={i} className="text-yellow-300" />
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          )
+        )}
+      </div>
+    </div>
+  );
+}
